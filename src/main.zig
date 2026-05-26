eba994c10ffce7d1a7bbe5940b88bfd480d6d2cf
Luc Alexander <lucskyalex@gmail.com>
2026-05-26 16:57:26 +0200

    unfinished interrupted tokenizer

---
+++ src/main.zig
@@ -0,0 +1,143 @@
+const std = @import("std");
+const Buffer = std.ArrayList;
+const Map = std.StringHashMap;
+
+const TOKEN = u64;
+
+const HOLE = '_';
+const QUOTE = '\'';
+const UNQUOTE = ',';
+const ADD = '+';
+const SUB = '-';
+const MUL = '*';
+const DIV = '/';
+const MOD = '%';
+const AND = '&';
+const OR = '|';
+const XOR = '^';
+const LT = '<';
+const GT = '>';
+const DEFINE = 0;
+const MACRO = 1;
+const UNIVERSE = 2;
+const CUT = 3;
+const HEAD = 4;
+const PROG = 5;
+const IF = 6;
+const INPUT = 7;
+const FLOAT = 8;
+const TAIL = 9;
+const LET = 10;
+const SET = 11;
+const LAMBDA = 12;
+const INT = 13;
+const NAT = 14;
+const LE = 15;
+const GE = 16;
+const EQ = 17;
+const NE = 18;
+const IDEN = 19;
+
+const Token = struct{
+	text: []const u8,
+	tag: TOKEN
+};
+
+pub fn tokenize(mem: *const std.mem.Allocator, text: []const u8) Buffer(Token) {
+	var tokens = Buffer(Token).init(mem.*);
+	var i: u64 = 0;
+	var tokmap = Map(TOKEN).init(mem.*);
+	tokmap.put("define", DEFINE ) catch unreachable;
+	tokmap.put("macro", MACRO ) catch unreachable;
+	tokmap.put("universe", UNIVERSE ) catch unreachable;
+	tokmap.put("cut", CUT ) catch unreachable;
+	tokmap.put("head", HEAD ) catch unreachable;
+	tokmap.put("prog", PROG ) catch unreachable;
+	tokmap.put("if", IF ) catch unreachable;
+	tokmap.put("input", INPUT ) catch unreachable;
+	tokmap.put("tail", TAIL ) catch unreachable;
+	tokmap.put("let", LET ) catch unreachable;
+	tokmap.put("set", SET ) catch unreachable;
+	tokmap.put("lambda", LAMBDA ) catch unreachable;
+	tokmap.put("<=", LE ) catch unreachable;
+	tokmap.put(">=", GE ) catch unreachable;
+	tokmap.put("==", EQ ) catch unreachable;
+	tokmap.put("!=", NE ) catch unreachable;
+	while (i < tokens.len){
+		const c = tokens[i];
+		switch(c){
+			' ', '\n', '\t', '\r' => {
+				i += 1;
+				continue;
+			},
+			HOLE, QUOTE, UNQUOTE, ADD, SUB, MUL, DIV, MOD, AND, OR, XOR, LT, GT => {
+				tokens.append(Token{
+					.text = text[i..i+1],
+					.tag = c
+				}) catch unreachable;
+				i += 1;
+				continue;
+			}
+			else => {
+				const k = i;
+				while ((std.ascii.isAlphanumeric(text[k]) or is_symbol(text[k])) and (k < tokens.len)){
+					k += 1;
+				}
+				if (tokmap.get(text[i..k])) |tok| {
+					tokens.append(Token{
+						.text = text[i..k],
+						.tag = tok
+					}) catch unreachable;
+					i = k;
+					continue;
+				}
+				_ = std.fmt.parseInt(i64, text[i..k], 10) catch {
+					
+				};
+			}
+		}
+	}
+}
+
+pub fn get_contents(mem: *const std.mem.Allocator, filename: []const u8) ![]u8 {
+	var infile = std.fs.cwd().openFile(filename, .{}) catch |err| {
+		std.debug.print("File not found: {s}\n", .{filename});
+		return err;
+	};
+	defer infile.close();
+	const stat = infile.stat() catch |err| {
+		std.debug.print("Errored file stat: {s}\n", .{filename});
+		return err;
+	};
+	const contents = infile.readToEndAlloc(mem.*, stat.size+1) catch |err| {
+		std.debug.print("Error reading file: {s}\n", .{filename});
+		return err;
+	};
+	return contents;
+}
+
+pub fn main() anyerror!void {
+	const heap = std.heap.page_allocator;
+	const main_buffer = heap.alloc(u8, 0x1000000) catch unreachable;
+	var main_mem_fixed = std.heap.FixedBufferAllocator.init(main_buffer);
+	var main_mem = main_mem_fixed.allocator();
+	const args = try std.process.argsAlloc(main_mem);
+	if (args.len == 1){
+		std.debug.print("-h for help\n", .{});
+		return;
+	}
+	if (std.mem.eql(u8, args[1], "-h")){
+		std.debug.print("Help Menu\n", .{});
+		std.debug.print("   -h : Show this message\n", .{});
+		std.debug.print("   [infile name] [outfile name]: compile file\n", .{});
+		return;
+	}
+	if (args.len < 3){
+		std.debug.print("-h for help\n", .{});
+		return;
+	}
+	const filename = args[1];
+	const outfile = args[2];
+	const contents = try get_contents(&main_mem, filename);
+	const tokens = tokenize(&main_mem, contents);
+}
